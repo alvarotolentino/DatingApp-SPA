f6e45a86ee57cc2f0cd90c6e58f561d6eccad67f
Alvaro Junior TOLENTINO URBINA <alvaro.tolentino@ibm.com>
2018-06-25 18:34:03 -0500

    Add Error Handling

--- src/app/_service/auth.service.ts
+++ src/app/_service/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions, Response } from '@angular/http';
-import { map } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable()
 export class AuthService {
@@ -19,20 +20,36 @@ export class AuthService {
             localStorage.setItem('token', user.tokenString);
             this.userToken = user.tokenString;
           }
-        })
+        }),
+        catchError(this.handleError)
       );
   }
 
   register(model: any) {
-    return this.http.post(
-      this.baseUrl + 'register',
-      model,
-      this.requestOptions()
-    );
+    return this.http
+      .post(this.baseUrl + 'register', model, this.requestOptions())
+      .pipe(catchError(this.handleError));
   }
 
   private requestOptions() {
     const headers = new Headers({ 'Content-type': 'application/json' });
     return new RequestOptions({ headers: headers });
+  }
+
+  private handleError(error: any) {
+    const applicationError = error.headers.get('Application-Error');
+    if (applicationError) {
+      return throwError(applicationError);
+    }
+    const serverError = error.json();
+    let modelStateErrors = '';
+    if (serverError) {
+      for (const key in serverError) {
+        if (serverError[key]) {
+          modelStateErrors += serverError[key] + '\n';
+        }
+      }
+    }
+    return throwError(modelStateErrors || 'Server error');
   }
 }
