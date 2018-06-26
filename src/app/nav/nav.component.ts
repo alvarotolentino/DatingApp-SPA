f52546a7fe7d98e6c4880ef24ec986a9b490cf46
Alvaro Tolentino <alvarotolentinou@gmail.com>
2018-06-26 07:47:22 -0500

    Add alertify service

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_service/auth.service';
+import { AlertifyService } from '../_service/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,17 +10,20 @@ import { AuthService } from '../_service/auth.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private alertify: AlertifyService
+  ) {}
 
   ngOnInit() {}
 
   login() {
     this.authService.login(this.model).subscribe(
       data => {
-        console.log('logged in successfully');
+        this.alertify.success('logged in successfully');
       },
       error => {
-        console.log('failed to login');
+        this.alertify.error(error);
       }
     );
   }
@@ -28,10 +32,10 @@ export class NavComponent implements OnInit {
     this.model = {};
     this.authService.userToken = null;
     localStorage.removeItem('token');
-    console.log('logged out');
+    this.alertify.message('logged out');
   }
 
-  loggedIn(){
+  loggedIn() {
     const token = localStorage.getItem('token');
     return !!token;
   }
