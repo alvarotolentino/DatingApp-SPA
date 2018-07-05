f806799286cb0a400415cfaf3cc6d08fa17bff38
Alvaro Junior TOLENTINO URBINA <alvaro.tolentino@ibm.com>
2018-07-05 16:09:29 -0500

    Add bootswatch and ngx-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HttpModule } from '@angular/http';
@@ -9,7 +10,6 @@ import { AuthService } from './_service/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { AlertifyService } from './_service/alertify.service';
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,7 +20,8 @@ import { AlertifyService } from './_service/alertify.service';
   imports: [
     BrowserModule,
     HttpModule,
-    FormsModule
+    FormsModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [
     AuthService,
