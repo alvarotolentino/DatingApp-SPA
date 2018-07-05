e6b02f9f96b11f5e8fdeb4ed23cf3b7917af0895
Alvaro Junior TOLENTINO URBINA <alvaro.tolentino@ibm.com>
2018-07-05 17:56:20 -0500

    Add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BsDropdownModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HttpModule } from '@angular/http';
@@ -10,23 +9,28 @@ import { AuthService } from './_service/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { AlertifyService } from './_service/alertify.service';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { RouterModule } from '../../node_modules/@angular/router';
+import { appRoutes } from './routes';
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [
-    AuthService,
-    AlertifyService
-  ],
+  providers: [AuthService, AlertifyService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
